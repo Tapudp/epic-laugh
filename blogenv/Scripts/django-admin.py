567b2199fd9f56701a2cbbc990dc278798dde7bd
Tapudp <divyeshpar2496@gmail.com>
2018-02-14 21:38:23 +0530

    added the virtual env created on the local system to show up on the pythonanywhere.com as well also we have to use pa_autoconfigure_django.py https://github.com/epic-laugh.git --nuke the nuke is to replace the previous app having and using the asame API that we create

---
+++ blogenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\divyesh\django-playlist\djangogirls\blogenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
